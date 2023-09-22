f4d9aadba94ec6d4f9dba49511b1d6cabb3c3bfa
Bilawal Hussain <117905020+bangash-0@users.noreply.github.com>
2023-09-22 16:15:35 +0500

    task-1

---
+++ assets/js/index_1.js
@@ -0,0 +1,8 @@
+let integer = 1;
+let boolean = true;
+let string = "1";
+let ud = undefined;
+let array = [1, 2, 3];
+let array_of_string = ["a", "b", "c"];
+let mix_Array = [1, "a", true];
+let object = { id: 1, name: "abc" };
