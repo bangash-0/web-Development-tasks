7058c6d3a324d52b4019bd5cebfb8818f52aafcb
Bilawal Hussain <117905020+bangash-0@users.noreply.github.com>
2023-09-22 16:54:19 +0500

    task-10

--- assets/js/index_10.js
+++ assets/js/index_10.js
@@ -5,17 +5,20 @@ function replaceMinWithNegativeOne(numbers) {
 
 	// Find the minimum value in the array
 	const minValue = Math.min(...numbers);
+	let occurrences = 0;
 
 	// Replace all occurrences of the minimum value with -1
 	for (let i = 0; i < numbers.length; i++) {
 		if (numbers[i] === minValue) {
 			numbers[i] = -1;
+			occurrences++;
 		}
 	}
 
-	return numbers;
+	return { numbers, occurrences };
 }
 
-const numbersList = [4, 2, 1, 2, 3, 1, 4];
+const numbersList = [4, 2, 1, 2, 3, 1, 4, 3];
 const result = replaceMinWithNegativeOne(numbersList);
-console.log(result);
+console.log(`Mutated Array ${result.numbers}`);
+console.log(`Occurrences of minimum value: ${result.occurrences}`);
