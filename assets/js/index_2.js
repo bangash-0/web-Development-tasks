ac181dffebab74cae97e78c376a22ddc1d8e9ee0
Bilawal Hussain <117905020+bangash-0@users.noreply.github.com>
2023-09-22 16:18:27 +0500

    task-2

---
+++ assets/js/index_2.js
@@ -0,0 +1,8 @@
+var str1 = "12345"; // String
+var num1 = Number(str1); // Number
+var int1 = parseInt(str1); // Integer
+var float1 = parseFloat(str1); // Float
+
+console.log("Number:", num1);
+console.log("parseInt:", int1);
+console.log("parseFloat:", float1);
