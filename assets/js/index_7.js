140739ef9bddacd1881ede5a43cdba6d2ac966a3
Bilawal Hussain <117905020+bangash-0@users.noreply.github.com>
2023-09-22 16:30:17 +0500

    task-7

---
+++ assets/js/index_7.js
@@ -0,0 +1,5 @@
+const reverseString = (str) => str.split("").reverse().join("");
+
+const originalString = "Hello, World!";
+const reversedString = reverseString(originalString);
+console.log(reversedString);
